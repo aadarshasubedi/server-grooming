6aeaee43c627444db8beb1942f22854427da4e7f
Thomas Adams <tjackadams@itadams.co.uk>
2012-09-12 17:41:42 +0100

    0.3
    - Added current user temporary internet files
    - Cleaned up code
    - Added OS version function

--- server-grooming/server-grooming/server-grooming.vb
+++ server-grooming/server-grooming/server-grooming.vb
@@ -1,38 +1,84 @@
-﻿Module mainmenu
+﻿Imports System
+Imports System.Management
+Module mainmenu
+    'Global Variables
+    Dim objcounter As ObjectModel.ReadOnlyCollection(Of String) 'Counter variable
+    Dim objsysenv As String = My.Application.GetEnvironmentVariable("windir") 'System directory variable
+    Dim objuserenv As String = My.Application.GetEnvironmentVariable("temp") 'Temp directory variable
+    Dim objprofenv As String = My.Application.GetEnvironmentVariable("userprofile") 'User profile variable
+    Function getosname() As String
+        Dim computer As String = String.Empty
+        Dim strResult As String = String.Empty
+        Dim OSVersion As String
+        Try
+            Dim searcher As New ManagementObjectSearcher(computer & "root\CIMV2", "SELECT * FROM Win32_OperatingSystem")
+            Dim colItems = searcher.Get()
+            For Each queryObj As ManagementObject In colItems
+                OSVersion = Left(queryObj("Version"), 3)
+                Select Case OSVersion
+                    Case "6.1"
+                        strResult = "Windows 7"
+                    Case "6.0"
+                        strResult = "Windows Vista"
+                    Case "5.2"
+                        strResult = "Windows 2003"
+                    Case "5.1"
+                        strResult = "Windows XP"
+                    Case "5.0"
+                        strResult = "Windows 2000"
+                    Case Else
+                        strResult = "Other Operating System"
+                End Select
+            Next
+            Return strResult
+        Catch err As ManagementException
+            Return "An error occurred while querying WMI: " & err.Message
+        Catch err As System.Runtime.InteropServices.COMException
+            If Not String.IsNullOrEmpty(computer) Then
+                Return ("An error occurred while connecting to computer " & computer.Replace("\", String.Empty))
+            Else
+                Return ("An error occurred: " & err.Message)
+            End If
+        End Try
+    End Function
+    '=============== Main Menu Start ===============
     Sub Main()
         Do
             Dim intInput As Integer = 0
+            'Clear console from previous commands
             Console.Clear()
             Console.WriteLine("Main Menu")
             Console.WriteLine("==========================")
             Console.WriteLine("1. Delete Windows Temporary Files")
             Console.WriteLine("2. Delete User Profile Temporary Files")
-            Console.WriteLine("3. Exit" & vbNewLine)
+            Console.WriteLine("3. Delete Temporary Internet Files")
+            Console.WriteLine("4. Exit" & vbNewLine)
             Console.Write("Enter your choice: ")
             If Integer.TryParse(Console.ReadLine(), intInput) Then
                 Select Case intInput
                     Case 1
-                        windir.windir()
+                        'Option 1
+                        mainmenu.subsystemp()
                     Case 2
-                        profiledir.profiledir()
+                        'Option 2
+                        mainmenu.subusertemp()
                     Case 3
+                        mainmenu.subtempintfiles()
+                    Case 4
+                        'Option 4
                         Exit Sub
                 End Select
             Else
                 Console.WriteLine("Please select from 1-2")
             End If
         Loop
+        '=============== Main Menu Finish ===============
     End Sub
-End Module
-Module windir
-    Sub windir()
-        Dim counter As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
-        Dim windirenv As String = My.Application.GetEnvironmentVariable("windir")
-        Dim wintempfiles As String = windirenv & "\Temp\*"
-        Dim wintemp As String = windirenv & "\Temp\"
-        Dim Files As String() = IO.Directory.GetFiles(wintemp)
-        counter = My.Computer.FileSystem.GetFiles(wintemp)
-        If counter.Count <= 0 Then
+    Sub subsystemp()
+        Dim objsysenvdir As String = objsysenv & "\Temp\"
+        Dim Files As String() = IO.Directory.GetFiles(objsysenvdir)
+        objcounter = My.Computer.FileSystem.GetFiles(objsysenvdir)
+        If objcounter.Count <= 0 Then
             Console.WriteLine("There are no files to delete")
             Console.WriteLine("Press Enter to return to the main menu")
             Console.ReadLine()
@@ -40,28 +86,26 @@ Module windir
             mainmenu.Main()
         Else
             Console.WriteLine("Deleting Windows Temporary Files")
-            For Each foundFile As String In My.Computer.FileSystem.GetFiles(wintemp)
-                    Console.WriteLine(foundFile)
+            For Each foundFile As String In My.Computer.FileSystem.GetFiles(objsysenvdir)
+                Console.WriteLine(foundFile)
                 Try
-                    Catch ex As System.IO.IOException
-                        System.IO.File.Delete(foundFile)
-                    End Try
+                Catch ex As System.IO.IOException
+                    System.IO.File.Delete(foundFile)
+                End Try
             Next
             Console.WriteLine("Windows Temporary Files Deleted")
             Console.WriteLine("Press Enter to return to the main menu")
             Console.ReadLine()
         End If
+        'Clear the memory
+        objsysenvdir = Nothing
+        Files = Nothing
+        objcounter = Nothing
     End Sub
-End Module
-Module profiledir
-    Sub profiledir()
-        Dim counter As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
-        Dim profenv As String = My.Application.GetEnvironmentVariable("temp")
-        Dim proftempfiles As String = profenv & "\*"
-        Dim proftemp As String = profenv
-        Dim Files As String() = IO.Directory.GetFiles(proftemp)
-        counter = My.Computer.FileSystem.GetFiles(proftemp)
-        If counter.Count <= 0 Then
+    Sub subusertemp()
+        Dim Files As String() = IO.Directory.GetFiles(objuserenv)
+        objcounter = My.Computer.FileSystem.GetFiles(objuserenv)
+        If objcounter.Count <= 0 Then
             Console.WriteLine("There are no files to delete")
             Console.WriteLine("Press Enter to return to the main menu")
             Console.ReadLine()
@@ -69,7 +113,7 @@ Module profiledir
             mainmenu.Main()
         Else
             Console.WriteLine("Deleting Current User Profile Temporary Files")
-            For Each foundFile As String In My.Computer.FileSystem.GetFiles(proftemp)
+            For Each foundFile As String In My.Computer.FileSystem.GetFiles(objuserenv)
                 Console.WriteLine(foundFile)
                 Try
                 Catch ex As System.IO.IOException
@@ -80,5 +124,42 @@ Module profiledir
             Console.WriteLine("Press Enter to return to the main menu")
             Console.ReadLine()
         End If
+        'Clear the memory
+        Files = Nothing
+        objcounter = Nothing
+    End Sub
+    Sub subtempintfiles()
+        Dim OSType = getosname()
+        Dim tempintfiles = Nothing
+        'Find the temporary internet file path for different operating systems
+        If OSType = "Windows 7" Or OSType = "Windows Vista" Then
+            tempintfiles = objprofenv & "\AppData\Local\Microsoft\Windows\Temporary Internet Files"
+        ElseIf OSType = "Windows 2003" Or OSType = "Windows XP" Then
+            tempintfiles = objprofenv & "\Local Settings\Temporary Internet Files"
+        End If
+        Dim Files As String() = IO.Directory.GetFiles(tempintfiles)
+        objcounter = My.Computer.FileSystem.GetFiles(tempintfiles)
+        If objcounter.Count <= 0 Then
+            Console.WriteLine("There are no files to delete")
+            Console.WriteLine("Press Enter to return to the main menu")
+            Console.ReadLine()
+            Exit Sub
+            mainmenu.Main()
+        Else
+            Console.WriteLine("Deleting Current User Profile Temporary Files")
+            For Each foundFile As String In My.Computer.FileSystem.GetFiles(tempintfiles)
+                Console.WriteLine(foundFile)
+                Try
+                Catch ex As System.IO.IOException
+                    System.IO.File.Delete(foundFile)
+                End Try
+            Next
+            Console.WriteLine("Current User Profile Temporary Files Deleted")
+            Console.WriteLine("Press Enter to return to the main menu")
+            Console.ReadLine()
+        End If
+        'Clear the memory
+        Files = Nothing
+        objcounter = Nothing
     End Sub
 End Module
